198c7637b7dcb10c31ac83e960cfa710b2eec128
Anton Kholomiov <anton.kholomiov@gmail.com>
2012-05-13 14:10:30 +0400

    added shortcuts for drum-notes

--- src/Temporal/Music/Note.hs
+++ src/Temporal/Music/Note.hs
@@ -3,14 +3,20 @@
 -- | This module gives an example of complete musical structure.
 -- It defines the notion of note. 
 module Temporal.Music.Note(
+        -- * Types
         Note(..), note,
-        Drum(..), drum, bam
+        Drum(..), drum, bam,
+        -- * Drum shortcuts
+        -- | Time stretched variants of function 'bam'.        
+        bd, wd, hd, qd, ed, sd, td, 
+        dbd, dwd, dhd, dqd, ded, dsd, dtd
     )
 where
 
 import Data.Finite
 import Temporal.Media(temp)
-import Temporal.Music.Track(Score, accent)
+import Temporal.Music.Track(Score, accent, dot,
+        bn, wn, hn, qn, en, sn, tn)
 import Temporal.Music.Volume
 import Temporal.Music.Pitch
 
@@ -63,3 +69,32 @@ drum vol = Drum (fromLevel vol) Nothing
 -- | Constructs drum note with given accent. Level is set to medium value.
 bam :: (HasDiap vol, Finite vol) => Accent -> Score (Drum vol a)
 bam a = accent a $ temp $ Drum mediumVolume Nothing
+
+--------------------------------------------------------------
+-- time stretching 
+--
+
+bd, wd, hd, qd, ed, sd, td :: 
+    (HasDiap vol, Finite vol) => Accent -> Score (Drum vol a)
+
+bd = bn . bam
+wd = bam
+hd = hn . bam
+qd = qn . bam
+ed = en . bam
+sd = sn . bam 
+td = tn . bam
+
+dbd, dwd, dhd, dqd, ded, dsd, dtd :: 
+    (HasDiap vol, Finite vol) => Accent -> Score (Drum vol a) 
+
+dbd = dot . bd
+dwd = dot . wd
+dhd = dot . hd
+dqd = dot . qd
+ded = dot . ed
+dsd = dot . sd
+dtd = dot . td
+
+
+
